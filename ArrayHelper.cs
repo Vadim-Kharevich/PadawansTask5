35ae0c6cf07328a639ec89b80547d4fbc4ffac45
Vadim-Kharevich <41491334+Vadim-Kharevich@users.noreply.github.com>
2019-02-13 19:58:13 +0300

    Update ArrayHelper.cs

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -6,8 +6,26 @@ namespace PadawansTask5
     {
         public static string CheckIfSymmetric(int[] source)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (source.Length % 2 != 0)
+            {
+                return "No";
+            }
+            int[] mas = new int[source.Length / 2];
+            int a = -1;
+            for(int i = source.Length / 2; i < source.Length; i++)
+            {
+                a++;
+                mas[a] = source[i];
+            }
+            Array.Reverse(mas);
+            for (int i = 0; i < source.Length/2; i++)
+            {
+                if(mas[i] != source[i])
+                {
+                    return "No";
+                }
+            }
+            return "Yes";
         }
     }
 }
